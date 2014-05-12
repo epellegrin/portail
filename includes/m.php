675de2d3db79e68c1e6b252c4b9c477ba798ce03
root <root@vps60795.ovh.net>
2014-05-12 11:03:53 +0200

    maj

--- includes/m.php
+++ includes/m.php
@@ -1,26 +1,22 @@
 <?php
-<<<<<<< HEAD
-$referer=$ _SERVER[ 'HTTP_REFERER'];
-$URL='http://vps60795.ovh.net' ;
-=======
 $referer=$_SERVER[ 'HTTP_REFERER'];
-$URL='http://localhost' ;
->>>>>>> 330e6cd4448f729650e50b0756dab1c1bc51001c
-$pos=s trrpos($referer,$URL);
+$URL='http://vps60795.ovh.net' ;
+$pos=strrpos($referer,$URL);
 if(empty($referer) || empty($pos) || $pos<0 ){
     header( 'Location: '.$URL); }
-$name=t rim($_POST[ 'name']);
-$email=t rim($_POST[ 'email']);
-$subject=t rim($_POST[ 'subject']);
-$betatest=t rim($_POST[ 'betatest']);
+$name=trim($_POST[ 'name']);
+$email=trim($_POST[ 'email']);
+$subject=trim($_POST[ 'subject']);
+$betatest=trim($_POST[ 'betatest']);
 if(function_exists( 'stripslashes')) {
-    $message=s tripslashes(trim($_POST[ 'message'])); }
+    $message=stripslashes(trim($_POST[ 'message'])); }
 else {
-    $message=t rim($_POST[ 'message']);
+    $message=trim($_POST[ 'message']);
 }
 $emailTo='contact@mobvino.com' ;
 $subject='Message depuis le site web de : ' .$name;
-$body="Nom: $name \n\nEmail : $email \n\nSujet : $subject\n\nMessage : $message\n\nDeveir beta testeur : $betatest" ; $headers='From: Mobvino <' .$emailTo. '>' . "\r\n" . 'Reply-To: ' . $email;
+$body="Nom: $name \n\nEmail : $email \n\nSujet : $subject\n\nMessage : $message\n\nDeveir beta testeur : $betatest" ; 
+$headers='From: Mobvino <' .$emailTo. '>' . "\r\n" . 'Reply-To: ' . $email;
 mail($emailTo, $subject, $body, $headers);
 return true;
 ?>
